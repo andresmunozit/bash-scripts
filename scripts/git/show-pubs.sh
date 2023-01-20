45b2cb52566bc5909f2775d718314ae75f83417f
Andres Muñoz <am@andresmunozit.com>
2023-01-19 19:10:56 -0500

    feature(git): update the script for showing also the filenames

--- scripts/git/show-pubs.sh
+++ scripts/git/show-pubs.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# Shows the content of every pub file that contains the ".com" string in the name
-ls -l -a ~/.ssh | grep .pub | grep .com | awk -v home="$HOME" '{print home"/.ssh/"$9}' | xargs cat | sed 's/$/\n/'
+
+PUB_KEYS=$(ls -l -a ~/.ssh | grep .pub | grep .com | awk -v home="$HOME" '{print home"/.ssh/"$9}')
+
+for key in $PUB_KEYS; do \
+  echo "Path: $key"
+  echo "Content:"
+  cat "$key" && echo
+done
